4031ec4e9b36ac32346acab6a3e3268df48e2a69
Crowdbotics <team@crowdbotics.com>
2023-05-02 12:10:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-grass-40397",
+  url: "https://falling-grass-40397.botics.co",
+  api: "https://falling-grass-40397.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
